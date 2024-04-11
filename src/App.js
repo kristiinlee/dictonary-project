ff4cb27804cd086077cf6c3ef70f5f4dac8d20fd
kristiinlee <kristiinlee@gmail.com>
2024-04-11 16:36:28 -0400

    Added search engine and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo.jpg";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+      
+        <Dictionary />
+      
+      
+      </div>
+      <footer className="App-footer">
+        This page was coded by <a href="https://github.com/kristiinlee">Kristin Jones</a> and open-sourced on <a href="https://github.com/kristiinlee/dictonary-project">Github</a>
+      </footer>
     </div>
   );
 }
