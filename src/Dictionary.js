6de4f8ee314e89091871e8cd0a30b9f8024b866c
kristiinlee <kristiinlee@gmail.com>
2024-04-22 07:31:52 -0400

    Added photos section

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,29 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("hello");
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response) {
         setResults(response.data[0]);
     }
 
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
+    }
+
     function search() {
         let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey = "MKGbi3dKsKe36jZhkhYfYABZ27xMalcbPHBpvtlIQxJIGXSwesRi9j34";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        axios.get(pexelsApiUrl, {headers: { Authorization: `${pexelsApiKey}` },}).then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -43,6 +53,7 @@ export default function Dictionary() {
                     suggested words: sunset, wine, book, plant...
                 </div>
                 <Results results={results} />
+                <Photos photos={photos} />
             </section>
         </div> 
     );
